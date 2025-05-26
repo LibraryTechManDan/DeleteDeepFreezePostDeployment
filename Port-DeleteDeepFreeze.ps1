b8e7b6e9f6da424bc68d023a76da8a850d571d13
LibraryTechManDan <daniel.palmer@cityofpae.sa.gov.au>
2025-05-26 17:04:22 +0930

    Update Port-DeleteDeepFreeze.ps1

--- Port-DeleteDeepFreeze.ps1
+++ Port-DeleteDeepFreeze.ps1
@@ -1,6 +1,6 @@
-# PowerShell Script: Delete File and Refresh Desktop
+# PowerShell Script: Delete File and Refresh Desktop Properly
 
-# Path to the file
+# Define path to the file
 $filePath = "C:\Users\Library\Desktop\FullFWAWebInstaller_Port Public PC Test.exe"
 
 # Delete the file if it exists
@@ -11,7 +11,26 @@ if (Test-Path $filePath) {
     Write-Output "File not found: $filePath"
 }
 
-# Refresh the desktop
-# This uses Windows API via COM object to send a refresh
-$shellApp = New-Object -ComObject Shell.Application
-$shellApp.NameSpace(0).Self.InvokeVerb("Refresh")
+# Add necessary user32 functions to send refresh command
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+
+public class WinAPI {
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+}
+"@
+
+# Constants
+$WM_COMMAND = 0x111
+$REFRESH = 0x7103
+
+# Get handle to the desktop
+$desktopHandle = [WinAPI]::FindWindow("Progman", $null)
+
+# Send refresh command to desktop
+[WinAPI]::SendMessage($desktopHandle, $WM_COMMAND, [IntPtr]$REFRESH, [IntPtr]0)
